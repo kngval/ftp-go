389bcc2626e53c9b18d585686d90beb955118cfb
kngval <bkvalteocs@gmail.com>
2026-08-09 08:22:47 -0700

    icons, adjustments

--- client/src/panels/LocalPane.tsx
+++ client/src/panels/LocalPane.tsx
@@ -1,10 +1,17 @@
 import './styles/pane.css' 
+import LocalIcon from '../assets/icons/user';
 const LocalPane = () => {
   return (
     <div className="pane" >
 
       <div className='header'>
-        <span className='panel-label'>Local</span>
+        <div className='icon-label-wrapper'>
+        <span className='local-icon'>
+          <LocalIcon size={20}/>
+        </span>
+        <span className='panel-label'>Local <span className='slash'>/</span></span>
+        </div>
+        <div>Projects</div>
       </div>
 
       <div>qweq</div>

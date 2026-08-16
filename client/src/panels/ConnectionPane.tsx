5b743519670f2be422a44620b5df7f184a2cf05d
kngval <bkvalteocs@gmail.com>
2026-08-16 16:26:48 -0700

    connection grid

--- client/src/panels/ConnectionPane.tsx
+++ client/src/panels/ConnectionPane.tsx
@@ -1,12 +1,12 @@
+import "./styles/connection_pane.css"
 const ConnectionPane: React.FC = () => {
   return (
     <div style={connectionWrapperStyles}>
-      <div style={connectionGridStyle}>
-        <div>Connection</div>
-        <div>Connection</div>
-        <div>Connection</div>
-        <div>Connection</div>
-        <div>Connection</div>
+      <div className="connection-grid" style={connectionGridStyle}>
+        <div>FTP</div>
+        <div>address</div>
+        <div>password</div>
+        <div>Connect Btn</div>
       </div>
     </div>
   );
@@ -20,6 +20,7 @@ const connectionWrapperStyles: React.CSSProperties = {
 
 const connectionGridStyle: React.CSSProperties = {
   display: "grid",
-  gridTemplateColumns: "1fr"
+  gridTemplateColumns: "1fr 1fr 1fr 1fr",
+  gap: "5px"
 };
 export default ConnectionPane;
